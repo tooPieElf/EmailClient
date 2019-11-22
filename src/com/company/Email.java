bd1e4dbb786b01016101aadc1f5e44d56d26d256
tooPieElf <ndimnezechukwudi@gmail.com>
2019-11-22 03:27:57 +0100

    set up department method

--- src/com/company/Email.java
+++ src/com/company/Email.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Email {
   private String firstname;
   private String lastname;
@@ -8,13 +10,23 @@ public class Email {
   private int mailBoxCapacity;
   private String alternateEmail;
 
-  public Email(String firstname, String lastname, String password, String department,
-      int mailBoxCapacity, String alternateEmail) {
+  public Email(String firstname, String lastname) {
     this.firstname = firstname;
     this.lastname = lastname;
-    this.password = password;
-    this.department = department;
-    this.mailBoxCapacity = mailBoxCapacity;
-    this.alternateEmail = alternateEmail;
+    this.department = setDepartment();
+    System.out.println("Email created "+ this.firstname+" "+this.lastname);
+// call department and return department.
+  }
+
+  private  String setDepartment() {
+
+    System.out.println("enter the students department please \n1 for sales \n 2 for department"
+        + "\n 3 for Acconting\n 0 for Name");
+    Scanner in =new Scanner(System.in);
+    int deptChoice = in.nextInt();
+    if(deptChoice==1){return "sales";}
+    else if(deptChoice==2){return "developement";}
+    else if(deptChoice ==3){return "Account";}
+    else{return "";}
   }
 }
