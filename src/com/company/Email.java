64c05388258b13aa30cff35028c503ff29aedce2
tooPieElf <ndimnezechukwudi@gmail.com>
2019-11-22 10:43:45 +0100

    setup automatic email generator;

--- src/com/company/Email.java
+++ src/com/company/Email.java
@@ -19,17 +19,19 @@ public class Email {
     this.lastname = lastname;
     this.department = setDepartment();
     this.password = setPassword(15);
+    this.alternateEmail = setEmail();
     System.out.println("Email created "+ this.firstname+" "+this.lastname);
     System.out.println("Department is "+ this.department);
     System.out.println("Passwod is "+ this.password);
+    System.out.println("email is "+ this.alternateEmail);
 
 
   }
 
   private  String setDepartment() {
 
-    System.out.println("DEPARTMENT CODES:  \n1 for sales \n 2 for department"
-        + "\n 3 for Acconting\n 0 for Name\n enter the students department please\n");
+    System.out.print("DEPARTMENT CODES:  \n1 for sales \n 2 for department"
+        + "\n 3 for Acconting\n 0 for Name\n enter the students department please");
     Scanner in =new Scanner(System.in);
     int deptChoice = in.nextInt();
     if(deptChoice==1){return "sales";}
@@ -41,12 +43,14 @@ public class Email {
     String seed = "abcdefghijklmnopqrstuvwxyzABCDEFHIJKLMNOPQRSTUVWXYZ@##$%^&*(){}><?"+this.firstname+this.lastname;
     char[] value = new char[passwordLength];
     for(int i =0;i<passwordLength;i++){
-      int randNum = (int) (Math.random()*seed.length());
+      int randNum = (int) (Math.random()*seed.length());// note the position of the brackets. it could be buggy.
       value[i]=seed.charAt(randNum);
-      System.out.println(randNum);
     }
     return  new String(value);
   }
+// Next task is to auto generate email based on the username.
+private String setEmail(){
 
-
+    return this.lastname+"."+this.firstname+"@"+"gmail.com";
+}
 }
