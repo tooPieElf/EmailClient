5086529fbb73cf35f5982b533fd59edd5135e33f
tooPieElf <ndimnezechukwudi@gmail.com>
2019-11-22 03:06:39 +0100

    initialized all email class fields

--- src/com/company/Email.java
+++ src/com/company/Email.java
@@ -1,5 +1,14 @@
 package com.company;
 
 public class Email {
+  private String firstname;
+  private String lastname;
+  private String password;
+  private String department;
+  private int mailBoxCapacity;
+  private String alternateEmail;
+
+
+
 
 }
